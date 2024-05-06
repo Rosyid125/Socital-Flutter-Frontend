eda7677c1e5cc74627e68d3233d3c8e4a963fb1b
Rosyid125 <rosyidferdiansyah1@gmail.com>
2024-05-06 20:45:09 +0700

    fetch data for posts (undone)

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,55 +1,60 @@
 import 'package:flutter/material.dart';
-import 'package:socital/styles.dart';
-import 'package:socital/widget/custom_texfield.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class HomeScreen extends StatefulWidget {
-  const HomeScreen({Key? key}) : super(key: key);
-
   @override
   _HomeScreenState createState() => _HomeScreenState();
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  List<dynamic> posts = [];
+
+  @override
+  void initState() {
+    super.initState();
+    fetchPosts();
+  }
+
+  Future<void> fetchPosts() async {
+    try {
+      final response = await http.get(
+          Uri.parse(
+              'http://127.0.0.1:8000/api/1/allposts'), // Ganti dengan URL API Anda
+          headers: {'Content-Type': 'application/json'});
+
+      if (response.statusCode == 200) {
+        setState(() {
+          posts = json.decode(response.body)['posts'];
+        });
+      } else {
+        throw Exception('Failed to load posts');
+      }
+    } catch (error) {
+      print(error.toString());
+      // Handle error
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.vertical(
-            bottom: Radius.circular(20),
-          ),
-        ),
-        backgroundColor: AppColors.kindaYellow,
-        title: Text(
-          'Home',
-          style: TextStyles.title,
-        ),
-        centerTitle: true,
-        elevation: 0,
+        title: Text('All Posts'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              'Welcome to Home Screen of Socital',
-              style: TextStyles.body,
-              textAlign: TextAlign.center,
-            ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                // Implement logout functionality here
-                // For example, navigate back to the login screen
-                Navigator.pushReplacementNamed(context, '/login');
+      body: posts.isEmpty
+          ? Center(
+              child: CircularProgressIndicator(),
+            )
+          : ListView.builder(
+              itemCount: posts.length,
+              itemBuilder: (BuildContext context, int index) {
+                return ListTile(
+                  title: Text(posts[index]['post']),
+                  // Jika ingin menampilkan gambar, tambahkan widget Image di sini
+                );
               },
-              child: Text(
-                'Logout',
-              ),
             ),
-          ],
-        ),
-      ),
     );
   }
 }
--- lib/utils/api_endpoints.dart
+++ lib/utils/api_endpoints.dart
@@ -1,10 +1,15 @@
 class ApiEndPoints {
   static final String baseUrl = "http://localhost:8000/api/";
   static _AuthEndPoints authEndpoints = _AuthEndPoints();
+  static _PostEndPoints postEndpoints = _PostEndPoints();
 }
 
 class _AuthEndPoints {
   final String register = 'register';
   final String login = 'login';
   final String me = 'me';
+}
+
+class _PostEndPoints {
+  final String posts = 'posts';
 }
